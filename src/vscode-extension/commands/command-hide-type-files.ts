c994194819ac4577624b31ed97b648d1765f8147
Phil Hill <phil@technobuddha.com>
2026-05-28 10:55:44 -0400

    work

--- src/vscode-extension/commands/command-hide-type-files.ts
+++ src/vscode-extension/commands/command-hide-type-files.ts
@@ -4,12 +4,12 @@ import { config } from '../extension.ts';
 
 export function commandHideTypeFiles(): Disposable {
   return commands.registerCommand('cmtd.typeFiles.hide', async () => {
-    const pattern = `**/${config.globIsTypeDefinition}`;
-
-    const wsConfig = workspace.getConfiguration();
-    window.showInformationMessage('Type files will now be hidden in the explorer.');
-
-    wsConfig.update('cmtd.showTypeFiles', false);
-    wsConfig.update('files.exclude', { [pattern]: true });
+    for (const folder of workspace.workspaceFolders ?? []) {
+      const pattern = `**/${config.globIsTypeDefinition(folder)}`;
+      const wsConfig = workspace.getConfiguration(undefined, folder);
+      window.showInformationMessage('Type files will now be hidden in the explorer.');
+      wsConfig.update('cmtd.showTypeFiles', false);
+      wsConfig.update('files.exclude', { [pattern]: true });
+    }
   });
 }
