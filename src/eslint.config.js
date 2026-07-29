d8341193d97fedd67eb77bcbac267051f1117909
Phil Hill <phil@technobuddha.com>
2026-07-29 17:59:24 -0400

    project

--- src/eslint.config.js
+++ src/eslint.config.js
@@ -6,7 +6,7 @@ import { lint } from '@technobuddha/project';
 
 export default lint({
   files: ['**/*.ts'],
-  ignores: ['@types/**/*'],
+  ignores: ['@types/**'],
   platform: 'node',
   typescript: true,
 });
